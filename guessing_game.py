b9a53d20ed4c1d935de3a50f9006615fde4be19e
minnepicker <minnepicker@users.noreply.github.com>
2018-10-04 07:23:23 +0800

    My code (pre-peer review)
    As far as I can tell, this will achieve the "exceeds" rating.

--- guessing_game.py
+++ guessing_game.py
@@ -2,36 +2,59 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces.
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
-
 import random
 
 
-def start_game():
-    """Psuedo-code Hints
+def start_game(high_score):
+    answer = random.randint(1, 100)
+    guess_count = 1
+    
+    print("\nWelcome to The Number Guessing Game!!! Here's how it works:\n1. You guess a number between 1-100.\n2. I'll tell you if the number is higher or lower than your guess.\n3. Rinse & repeat. Keep guessing until you find the answer. I'll tell you how many guesses you took.")
+    # Prevents high_score from being displayed until after first game
+    if high_score < 100:
+        print("The best so far is {} guesses. Can you do it in less?".format(high_score))
+    
+    
+    while True:
+        guess = input("\nWhat's your guess? ")
+        
+        
+        try:
+            guess = int(guess)
+            # Handles gueeses outside 1-100
+            if guess > 100 or guess < 1:
+                raise ValueError()
+        # Handles guesses that aren't numbers
+        except ValueError:
+            print("Sorry, you have to type digits between 1-100. ")
+            continue
+        else:
+            if guess == answer:
+                print("Got it! It took you {} guesses.".format(guess_count))
+                # Checks for new high_score
+                if guess_count < high_score:
+                    high_score = guess_count
+                break
+            elif guess > answer:
+                print("It's lower")
+                guess_count += 1
+                continue
+            else:
+                print("It's higher")
+                guess_count += 1
+                continue
+    
+    
+    print("\nThank you for playing!")
+    
+    # Checks whether to keep playing or not.
+    keep_playing = input("Would you like to play again? Yes/No ")
+    if keep_playing.lower() == "yes" or keep_playing.lower() == "y":
+        start_game(high_score)
+    else:
+        print("See you next time!")
 
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
+# High score starts at 100, it should never take more than 100 guesses to get the right answer.
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
-    start_game()
+    start_game(100)
